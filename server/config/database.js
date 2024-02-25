ede6d9b1584923be9c5ab3419f2d8c652c3c434d
Shayan Irfan <65041222+ShayanIrfan@users.noreply.github.com>
2024-02-25 07:08:31 +0500

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'mongodb+srv://webnersblockchain:aBKzuJPDNlm6o8WE@cluster0.bcuuaop.mongodb.net/charting_6278?retryWrites=true&w=majority',
+  url: process.env.MONGO_URL
 };
